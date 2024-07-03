c5c1dac6b6899bbbcc904431791ab8c8b0afc090
keekyPo_DRUD_IL <eidenblock@tutamail.com>
2024-07-03 23:16:22 +0300

    added scss

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
+<style lang="scss">
+  @import "@/assets/scss/global";
+  @import "@/assets/scss/partials/app-container";
+</style>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+const appConfig = useAppConfig();
+console.log('APP_CONFIG.TITTLE', appConfig.tittle);
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <main class="app-container">
+    <NuxtPage />
+  </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "assets/scss/vars.scss";'
+        }
+      }
+    }
+  },
+
+  devtools: {
+    enabled: true
+  },
+
+  runtimeConfig: {
+
+  },
+
+  $production: {
+
+  },
+
+  $development: {
+    routeRules: {}
+  }
 })
